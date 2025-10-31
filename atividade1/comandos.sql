5f963acd18220076f4add082282efe1ccf888338
Vinicius Martins <vini.soares.martins@gmail.com>
2025-10-30 22:34:23 -0300

    Modify SQL tables and constraints in comandos.sql
    Updated table definitions for Time, Joga, and Partida. Increased saldo_gols size, added constraints, and created Partida table.

--- atividade1/comandos.sql
+++ atividade1/comandos.sql
@@ -2,19 +2,36 @@ CREATE TABLE Time (
   nome VARCHAR(30) PRIMARY KEY,
   estado VARCHAR(2),
   tipo VARCHAR(20) CHECK (tipo in ('AMADOR','PROFISSIONAL')),
-  saldo_gols NUMBER(3)
+  saldo_gols NUMBER(5)
 ); -- ok
 
 CREATE TABLE Joga (
   time1 VARCHAR(30),
   time2 VARCHAR(30),
   classico NUMBER(1) CHECK (classico in (0,1)),
-  CONSTRAINT pk_time PRIMARY KEY (time1, time2),
+  CONSTRAINT pk_joga PRIMARY KEY (time1, time2),
   CONSTRAINT fk_time1 FOREIGN KEY (time1)
     REFERENCES Time(nome)
-    ON DELETE SET CASCADE,
+    ON DELETE CASCADE,
+  CONSTRAINT fk_time2 FOREIGN KEY (time2)
+    REFERENCES Time(nome)
+    ON DELETE CASCADE,
+  CONSTRAINT ck_joga CHECK (time1 <> time2)
+); -- ok
+
+CREATE TABLE Partida (
+  time1 VARCHAR(30),
+  time2 VARCHAR(30),
+  data_partida DATE,
+  placar VARCHAR(7) DEFAULT '0x0',
+  local VARCHAR(30),
+  CONSTRAINT pk_partida PRIMARY KEY (time1, time2, data_partida),
+  CONSTRAINT fk_time1 FOREIGN KEY (time1)
+    REFERENCES Time(nome)
+    ON DELETE CASCADE,
   CONSTRAINT fk_time2 FOREIGN KEY (time2)
     REFERENCES Time(nome)
     ON DELETE CASCADE,
   CONSTRAINT ck_joga CHECK (time1 <> time2),
+  CONSTRAINT ck_placar CHECK (REGEXP_LIKE(placar, '^[0-9]{1,2,3}x[0-9]{1,2,3}$'))
 );
